bf282f7c156fb544c230c7741af89e7d46ae3222
alexiabungay <alexia_bungay@dlsu.edu.ph>
2026-08-13 19:34:03 +0800

    changed comments

--- generateSource.m
+++ generateSource.m
@@ -2,44 +2,30 @@ function sourceImage = generateSource(xGrid, yGrid, sourceType, ...
     xSource, ySource, rSource, customImg)
 
     if strcmp(sourceType, 'Gaussian Profile')
-
-        % ---------------------------------------------------------
-        % Gaussian Profile
-        % ---------------------------------------------------------
+        % Generate Gaussian light profile
         sourceImage = exp( ...
             -(((xGrid - xSource).^2 + ...
             (yGrid - ySource).^2) / ...
             (2 * rSource^2)));
-
     else
-
         if isempty(customImg)
-
-            % -----------------------------------------------------
-            % Fallback Gaussian if no image is loaded
-            % -----------------------------------------------------
+            % Generate fallback Gaussian profile
             sourceImage = exp( ...
                 -(((xGrid - xSource).^2 + ...
                 (yGrid - ySource).^2) / ...
                 (2 * rSource^2)));
-
         else
-
-            % -----------------------------------------------------
             % Convert uploaded image to grayscale
-            % -----------------------------------------------------
             if size(customImg, 3) == 3
                 imgGray = rgb2gray(customImg);
             else
                 imgGray = customImg;
             end
 
-            % Convert image to double [0,1]
+            % Convert image to double values from 0 to 1
             imgGray = double(imgGray) / 255.0;
 
-            % -----------------------------------------------------
-            % Keep the original image resolution
-            % -----------------------------------------------------
+            % Get original image dimensions
             [imageRows, imageCols] = size(imgGray);
 
             % Preserve the original image aspect ratio
@@ -56,15 +42,11 @@ function sourceImage = generateSource(xGrid, yGrid, sourceType, ...
 
             [imageXGrid, imageYGrid] = meshgrid(imageX, imageY);
 
-            % -----------------------------------------------------
-            % Position and size of the source
-            % -----------------------------------------------------
+            % Calculate source position and size
             queryX = (xGrid - xSource) / rSource;
             queryY = (yGrid - ySource) / rSource;
 
-            % -----------------------------------------------------
-            % Map the complete original image onto the grid
-            % -----------------------------------------------------
+            % Map the original image onto the simulation grid
             sourceImage = interp2( ...
                 imageXGrid, ...
                 imageYGrid, ...
